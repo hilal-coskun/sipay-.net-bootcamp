375844b76e3ea4a3e8379afe8ea8fe567da31c09
hilal.coskun <hilal.coskun20@gmail.com>
2023-08-05 01:15:46 +0300

    added controllers

--- SipayFinalProject/FinalProject/Controllers/DuesController.cs
+++ SipayFinalProject/FinalProject/Controllers/DuesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FinalProject.Business.Abstract;
+using FinalProject.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Controllers
@@ -7,5 +9,77 @@ namespace FinalProject.Controllers
 	[ApiController]
 	public class DuesController : ControllerBase
 	{
+		private IDuesService _duesService;
+
+		public DuesController(IDuesService duesService)
+		{
+			_duesService = duesService;
+		}
+
+		[HttpGet("getAll")]
+		public IActionResult GetList()
+		{
+			var result = _duesService.GetList();
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+
+			return BadRequest(result.Message);
+		}
+
+		[HttpGet("getListByFlats")]
+		public IActionResult GetListByFlats(int flatId)
+		{
+			var result = _duesService.GetListByFlat(flatId);
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+
+			return BadRequest(result.Message);
+		}
+
+
+		[HttpGet("getById")]
+		public IActionResult GetById(int id)
+		{
+			var result = _duesService.GetById(id);
+			if (result.Success)
+				return Ok(result.Data);
+
+			return BadRequest(result.Message);
+		}
+
+
+		[HttpPost("add")]
+		public IActionResult Add(Dues dues)
+		{
+			var result = _duesService.Add(dues);
+			if (result.Success)
+				return Ok(result.Message);
+
+			return BadRequest(result.Message);
+		}
+
+		[HttpPost("update")]
+		public IActionResult Update(Dues dues)
+		{
+			var result = _duesService.Update(dues);
+			if (result.Success)
+				return Ok(result.Message);
+
+			return BadRequest(result.Message);
+		}
+
+		[HttpPost("delete")]
+		public IActionResult Delete(Dues dues)
+		{
+			var result = _duesService.Delete(dues);
+			if (result.Success)
+				return Ok(result.Message);
+
+			return BadRequest(result.Message);
+		}
 	}
 }
--- SipayFinalProject/FinalProject/Controllers/InvoiceController.cs
+++ SipayFinalProject/FinalProject/Controllers/InvoiceController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FinalProject.Business.Abstract;
+using FinalProject.Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Controllers
@@ -7,5 +9,77 @@ namespace FinalProject.Controllers
 	[ApiController]
 	public class InvoiceController : ControllerBase
 	{
+		private IInvoiceService _invoiceService;
+
+		public InvoiceController(IInvoiceService invoiceService)
+		{
+			_invoiceService = invoiceService;
+		}
+
+		[HttpGet("getAll")]
+		public IActionResult GetList()
+		{
+			var result = _invoiceService.GetList();
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+
+			return BadRequest(result.Message);
+		}
+
+		[HttpGet("getListByFlats")]
+		public IActionResult GetListByFlats(int flatId)
+		{
+			var result = _invoiceService.GetListByFlat(flatId);
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+
+			return BadRequest(result.Message);
+		}
+
+
+		[HttpGet("getById")]
+		public IActionResult GetById(int id)
+		{
+			var result = _invoiceService.GetById(id);
+			if (result.Success)
+				return Ok(result.Data);
+
+			return BadRequest(result.Message);
+		}
+
+
+		[HttpPost("add")]
+		public IActionResult Add(Invoice ınvoice)
+		{
+			var result = _invoiceService.Add(ınvoice);
+			if (result.Success)
+				return Ok(result.Message);
+
+			return BadRequest(result.Message);
+		}
+
+		[HttpPost("update")]
+		public IActionResult Update(Invoice ınvoice)
+		{
+			var result = _invoiceService.Update(ınvoice);
+			if (result.Success)
+				return Ok(result.Message);
+
+			return BadRequest(result.Message);
+		}
+
+		[HttpPost("delete")]
+		public IActionResult Delete(Invoice ınvoice)
+		{
+			var result = _invoiceService.Delete(ınvoice);
+			if (result.Success)
+				return Ok(result.Message);
+
+			return BadRequest(result.Message);
+		}
 	}
 }
