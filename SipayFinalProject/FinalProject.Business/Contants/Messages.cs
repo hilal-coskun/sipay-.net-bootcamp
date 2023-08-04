173f230daa43c849a9997be2c9266efe23e324b1
hilal.coskun <hilal.coskun20@gmail.com>
2023-08-05 00:31:48 +0300

    added error and success messages

---
+++ SipayFinalProject/FinalProject.Business/Contants/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Business.Contants
+{
+	public class Messages
+	{
+		public static string UserAdded = "The user has been successfully registered";
+		public static string UserDeleted = "The user was successfully deleted";
+		public static string UserUpdated = "The user has been successfully updated";
+
+		public static string InvoiceAdded = "The invoice has been successfully registered";
+		public static string InvoiceDeleted = "The invoice was successfully deleted";
+		public static string InvoiceUpdated = "The invoice has been successfully updated";
+
+		public static string FlatAdded = "The flat has been successfully registered";
+		public static string FlatDeleted = "The flat was successfully deleted";
+		public static string FlatUpdated = "The flat has been successfully updated";
+
+		public static string DuesAdded = "The dues has been successfully registered";
+		public static string DuesDeleted = "The dues was successfully deleted";
+		public static string DuesUpdated = "The dues has been successfully updated";
+	}
+}
